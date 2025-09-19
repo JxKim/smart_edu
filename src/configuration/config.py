467493a8083bd6b4cb425a7afd54c9e5c66f6a21
LYH <3066136041@qq.com>
2025-09-19 16:35:26 +0800

    1111

---
+++ src/configuration/config.py
@@ -0,0 +1,13 @@
+MYSQL_CONFIG = {
+    'host': 'localhost',
+    'port': 3306,
+    'user': 'root',
+    'password': '123456',
+    'database': 'smart_edu'
+}
+
+NEO4J_CONFIG = {
+    'uri': "neo4j://localhost:7687",
+    'auth': ('neo4j', "lyh040102")
+}
+
--- src/datasync/utils.py
+++ src/datasync/utils.py
@@ -19,4 +19,27 @@ class Neo4jWriter:
     def __init__(self):
         self.driver = GraphDatabase.driver(**config.NEO4J_CONFIG)
 
-    def write_nodes(self, label: str, ):
+    def write_nodes(self, label: str, propeprties: list[dict]):
+        cypher = f"""
+                    UNWIND $batch AS item
+                    MERGE (n:{label} {{id:item.id, name:item.name}})
+                    """
+        self.driver.execute_query(cypher, batch = propeprties)
+
+    def write_relations(self, type: str, start_label, end_label, relations: list[dict]):
+        cypher = f"""
+                UNWIND $batch AS item
+                MATCH (start:{start_label} {{id:item.start_id}}),(end:{end_label} {{id:item.end_id}})
+                MERGE (start)-[:{type}]->(end)
+            """
+        self.driver.execute_query(cypher, batch=relations)
+
+if __name__ == '__main__':
+    reader = MysqlReader()
+    writer = Neo4jWriter()
+    sql = "SELECT id, category_name as name FROM base_category_info"
+    result = reader.read(sql)
+    writer.write_nodes("分类" , result)
+
+
+
