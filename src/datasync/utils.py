fb1fd6f16c24a72d044531102244d59fca1ba0ce
LYH <3066136041@qq.com>
2025-09-20 17:08:06 +0800

    9/20

--- src/datasync/utils.py
+++ src/datasync/utils.py
@@ -1,3 +1,5 @@
+import decimal
+
 import pymysql
 from neo4j import GraphDatabase
 from pymysql.cursors import DictCursor
@@ -12,34 +14,41 @@ class MysqlReader:
 
     def read(self, sql):
         self.cursor.execute(sql)
-        return self.cursor.fetchall()
+        rows = self.cursor.fetchall()
+
+        # 自动把 Decimal 转 float，避免 Neo4j 驱动报错
+        for row in rows:
+            for k, v in row.items():
+                if isinstance(v, decimal.Decimal):
+                    row[k] = float(v)
+        return rows
+
 
 
 class Neo4jWriter:
     def __init__(self):
         self.driver = GraphDatabase.driver(**config.NEO4J_CONFIG)
 
-    def write_nodes(self, label: str, propeprties: list[dict]):
-        cypher = f"""
-                    UNWIND $batch AS item
-                    MERGE (n:{label} {{id:item.id, name:item.name}})
-                    """
-        self.driver.execute_query(cypher, batch = propeprties)
+    def write_nodes(self, label: str, properties: list[dict]):
 
-    def write_relations(self, type: str, start_label, end_label, relations: list[dict]):
+
+        # 拼接所有属性
+        all_props = ", ".join([f"{k}: item.{k}" for k in properties[0].keys()])
         cypher = f"""
-                UNWIND $batch AS item
-                MATCH (start:{start_label} {{id:item.start_id}}),(end:{end_label} {{id:item.end_id}})
-                MERGE (start)-[:{type}]->(end)
-            """
+            UNWIND $batch AS item
+            CREATE (n:{label})
+            SET n += {{{all_props}}}
+        """
+        self.driver.execute_query(cypher, batch=properties)
+
+    def write_relations(self, type: str, start_label, end_label, relations: list[dict],
+                        start_key: str, end_key: str):
+        cypher = f"""
+        UNWIND $batch AS item
+        MATCH (start:{start_label} {{{start_key}: item.start_id}})
+        MATCH (end:{end_label} {{{end_key}: item.end_id}})
+        MERGE (start)-[:{type}]->(end)
+        """
         self.driver.execute_query(cypher, batch=relations)
-
 if __name__ == '__main__':
-    reader = MysqlReader()
-    writer = Neo4jWriter()
-    sql = "SELECT id, category_name as name FROM base_category_info"
-    result = reader.read(sql)
-    writer.write_nodes("分类" , result)
-
-
-
+    pass
