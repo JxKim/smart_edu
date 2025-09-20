9b166f14a56c843a570ae98dc0a1c937272d21a8
yanyixiao <yanyixiao@yyx.com>
2025-09-20 17:46:52 +0800

    数据清洗和创建neo4j图谱同步进行中

---
+++ src/configuration/config.py
@@ -0,0 +1,14 @@
+MYSQL_CONFIG = {
+    'host': 'localhost',
+    'port': 3306,
+    'user': 'root',
+    'password': '17751817',
+    'database': 'edu_ai'
+}
+
+NEO4J_CONFIG = {
+    'uri': 'neo4j://localhost:7687',
+    'auth': ('neo4j', '17751817')
+}
+
+ER_TOOL = 'dbdiagram.io'
