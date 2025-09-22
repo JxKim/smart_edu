48dc5bc8195e1bdc1e509e20c15ee411d04006ea
Leo12358 <2395066917@qq.com>
2025-09-22 13:15:46 +0800

    读取原始数据--在sql中读取--章节名称-数据

--- src/SQL_utils/load_from_sql.py
+++ src/SQL_utils/load_from_sql.py
@@ -1,8 +1,10 @@
 # 本py文件用于加载初始的 在sql中读取到的数据
 import pymysql
 from configurations import config
+import random
 
-def sql_loader():
+# 这个读取的是少样本的数据
+def sql_loader_less():
     connection=pymysql.connect(**config.SQL_CONFIG)
     with connection.cursor() as cursor:
         # language=sql
@@ -16,10 +18,36 @@ def sql_loader():
         results=cursor.fetchall()
         return results
 
-if __name__=="__main__":
-    results=sql_loader()
-    results=[res[0] for res in results]
-    course_introduce_results="\n".join(results)
+# 这个读取的是大量样本的数据
+def sql_loader_large():
+    connection=pymysql.connect(**config.SQL_CONFIG)
+    with connection.cursor() as cursor:
+        # language=sql
+        sql="""
+            select
+                chapter_name as text 
+            from 
+            chapter_info
+        """
+        cursor.execute(sql)
+        results=cursor.fetchall()
+        return results
 
-    with open(file=config.SQL_LOADER_PATH/"course_introduce.txt",mode="w",encoding="utf-8") as f:
-        f.write(course_introduce_results)
+if __name__=="__main__":
+    # 将读取到的少量数据写入文件
+    # results=sql_loader_less()
+    # results=[res[0] for res in results]
+    # course_introduce_results="\n".join(results)
+    #
+    # with open(file=config.SQL_LOADER_PATH/"course_introduce.txt",mode="w",encoding="utf-8") as f:
+    #     f.write(course_introduce_results)
+
+    # 将读取到的大量数据写入文件
+    results = sql_loader_large()
+    results = [res[0] for res in results]
+    results=random.sample(results, 500)
+
+    chapter_name_results = "\n".join(results)
+
+    with open(file=config.SQL_LOADER_PATH / "chapter_name.txt", mode="w", encoding="utf-8") as f:
+        f.write(chapter_name_results)
